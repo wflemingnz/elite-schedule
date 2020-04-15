bec32db50be06849b13e3a8ae748fe2d04000d1f
Wade Fleming <WFleming@caci.co.uk>
2020-04-15 13:47:24 +0100

    add navigation to tournaments page

--- src/app/my-teams/my-teams.page.ts
+++ src/app/my-teams/my-teams.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-teams.page.scss'],
 })
 export class MyTeamsPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
