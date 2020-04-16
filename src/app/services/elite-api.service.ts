c3da9473b49312c40a8b221be8019a7236fe0935
Wade Fleming <WFleming@caci.co.uk>
2020-04-16 16:59:31 +0100

    retrieve list of tournaments from api

--- src/app/services/elite-api.service.ts
+++ src/app/services/elite-api.service.ts
@@ -8,4 +8,8 @@ export class EliteApiService {
   private baseUrl = 'https://elite-schedule-app-4e079.firebaseio.com/';
 
   constructor(private http: HttpClient) {}
+
+  getTournaments() {
+    return this.http.get(`${this.baseUrl}data/tournaments.json`);
+  }
 }
--- src/app/tournaments/tournaments.page.ts
+++ src/app/tournaments/tournaments.page.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Location } from '@angular/common';
+import { EliteApiService } from '../services/elite-api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tournaments',
   templateUrl: './tournaments.page.html',
   styleUrls: ['./tournaments.page.scss'],
 })
-export class TournamentsPage implements OnInit {
-  constructor(private location: Location) {}
+export class TournamentsPage {
+  tournaments$ = this.apiService.getTournaments();
 
-  ngOnInit() {}
+  constructor(private apiService: EliteApiService) {}
 }
