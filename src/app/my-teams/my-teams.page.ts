3bcff162380806032fcc10620821ec09cadfb3e3
Wade Fleming <WFleming@caci.co.uk>
2020-04-17 18:20:28 +0100

    add my teams list

--- src/app/my-teams/my-teams.page.ts
+++ src/app/my-teams/my-teams.page.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-teams.page.scss'],
 })
 export class MyTeamsPage implements OnInit {
+  favourites = [
+    {
+      team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+      tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+      tournamentName: 'March Madness Tournament',
+    },
+    {
+      team: { id: 805, name: 'HC Elite', coach: 'Michelotti' },
+      tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+      tournamentName: 'Holiday Hoops Challenge',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
