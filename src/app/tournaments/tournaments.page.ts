bf73f1a81d21b246b2756beed2e7c1b450d18a09
Wade Fleming <WFleming@caci.co.uk>
2020-04-17 13:57:02 +0100

    add loading message to tournaments

--- src/app/tournaments/tournaments.page.ts
+++ src/app/tournaments/tournaments.page.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EliteApiService } from '../services/elite-api.service';
+import { LoadingController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tournaments',
   templateUrl: './tournaments.page.html',
   styleUrls: ['./tournaments.page.scss'],
 })
-export class TournamentsPage {
-  tournaments$ = this.apiService.getTournaments();
+export class TournamentsPage implements OnInit {
+  tournaments$: Observable<any>;
 
-  constructor(private apiService: EliteApiService) {}
+  constructor(
+    private apiService: EliteApiService,
+    private loadingController: LoadingController
+  ) {}
+
+  async ngOnInit() {
+    const loader = await this.loadingController.create({
+      message: 'Getting tournaments...',
+    });
+
+    await loader.present();
+    this.tournaments$ = this.apiService
+      .getTournaments()
+      .pipe(finalize(() => loader.dismiss()));
+  }
 }
