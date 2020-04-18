3b896d9f63391d19c31d2612097bd1bc0858d130
Wade Fleming <WFleming@caci.co.uk>
2020-04-18 20:20:38 +0100

    filter games by date WIP

--- src/app/team-detail/team-detail.page.ts
+++ src/app/team-detail/team-detail.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EliteApiService } from '../services/elite-api.service';
 import { Observable } from 'rxjs';
+import { filter, flatMap, toArray } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-team-detail',
@@ -11,9 +13,8 @@ import { Observable } from 'rxjs';
 export class TeamDetailPage implements OnInit {
   team$: Observable<any>;
   teamStanding$: Observable<any>;
-  games$: Observable<any>;
-  tournamentId: string;
-  filterDate: string;
+  gamesFilteredByDate$: ReturnType<EliteApiService['getGamesForTeam']>;
+  filterDate: string; // = '2015-03-21';
 
   constructor(
     private route: ActivatedRoute,
@@ -21,13 +22,19 @@ export class TeamDetailPage implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.tournamentId = this.route.snapshot.paramMap.get('tournamentId');
+    const tournamentId = this.route.snapshot.paramMap.get('tournamentId');
     const teamId = +this.route.snapshot.paramMap.get('teamId');
-    this.team$ = this.apiService.getTeam(this.tournamentId, teamId);
-    this.teamStanding$ = this.apiService.getTeamStanding(
-      this.tournamentId,
-      teamId
+    this.team$ = this.apiService.getTeam(tournamentId, teamId);
+    this.teamStanding$ = this.apiService.getTeamStanding(tournamentId, teamId);
+
+    const games$ = this.apiService.getGamesForTeam(tournamentId, teamId);
+    this.gamesFilteredByDate$ = games$.pipe(
+      flatMap((games) => games),
+      filter(
+        (game) =>
+          !this.filterDate || moment(game.time).isSame(this.filterDate, 'day')
+      ),
+      toArray()
     );
-    this.games$ = this.apiService.getGamesForTeam(this.tournamentId, teamId);
   }
 }
