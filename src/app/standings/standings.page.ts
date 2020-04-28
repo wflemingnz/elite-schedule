ca40a1604ce8d9fef9de9ab68c7eef20fa56ecb9
Wade Fleming <WFleming@caci.co.uk>
2020-04-28 22:28:02 +0100

    filter standings by division of current team

--- src/app/standings/standings.page.ts
+++ src/app/standings/standings.page.ts
@@ -5,6 +5,7 @@ import { EliteApiService } from '../services/elite-api.service';
 import { map, flatMap, filter, toArray, switchMap } from 'rxjs/operators';
 import { TeamStandingData } from '../models/team-standing';
 import * as _ from 'lodash';
+import { TeamData } from '../models/team';
 
 @Component({
   selector: 'app-standings',
@@ -23,17 +24,23 @@ export class StandingsPage implements OnInit {
 
   ngOnInit() {
     const tournamentId = this.route.snapshot.paramMap.get('tournamentId');
+    const teamId = +this.route.snapshot.paramMap.get('teamId');
+    const team$ = this.apiService.getTeam(tournamentId, teamId);
     this.standings$ = this.apiService.getTournamentTeamStandings(tournamentId);
     this.standingsOrderedByDivision$ = this.filterStandingsSubject.pipe(
       switchMap((filterMode) =>
-        this.standings$.pipe(
-          flatMap((standings) => standings),
-          filter((standing) =>
-            this.showStandingBasedOnFilter(standing, filterMode)
-          ),
-          toArray(),
-          map((standings) =>
-            _.sortBy(standings, (s: TeamStandingData) => s.division)
+        team$.pipe(
+          switchMap((team) =>
+            this.standings$.pipe(
+              flatMap((standings) => standings),
+              filter((standing) =>
+                this.showStandingBasedOnFilter(standing, filterMode, team)
+              ),
+              toArray(),
+              map((standings) =>
+                _.sortBy(standings, (s: TeamStandingData) => s.division)
+              )
+            )
           )
         )
       )
@@ -56,7 +63,11 @@ export class StandingsPage implements OnInit {
     this.filterStandingsSubject.next($event.target.value);
   }
 
-  showStandingBasedOnFilter(standing: TeamStandingData, filterMode: string) {
-    return filterMode === 'all' || standing.division === '5th White';
+  showStandingBasedOnFilter(
+    standing: TeamStandingData,
+    filterMode: string,
+    team: TeamData
+  ) {
+    return filterMode === 'all' || standing.division === team.division;
   }
 }
