851d88b53e6609f52c5930bab75ccb7da91303fe
Wade Fleming <WFleming@caci.co.uk>
2020-04-30 22:22:57 +0100

    add directions to game page

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { EliteApiService } from '../services/elite-api.service';
 import { GameData } from '../models/game';
+import { switchMap, map } from 'rxjs/operators';
+import { LocationData } from '../models/location';
+
+declare var window: any;
 
 @Component({
   selector: 'app-game',
@@ -13,6 +17,7 @@ export class GamePage implements OnInit {
   tournamentId: string;
   gameId: number;
   game$: Observable<GameData>;
+  location$: Observable<LocationData>;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,11 +28,20 @@ export class GamePage implements OnInit {
     this.tournamentId = this.route.snapshot.paramMap.get('tournamentId');
     this.gameId = +this.route.snapshot.paramMap.get('gameId');
     this.game$ = this.apiService.getGame(this.tournamentId, this.gameId);
+    this.location$ = this.game$.pipe(
+      switchMap((game) =>
+        this.apiService
+          .getTournamentLocations(this.tournamentId)
+          .pipe(map((locations) => locations[game.locationId]))
+      )
+    );
   }
 
   getScoreColor(score1: string, score2: string) {
     return +score1 > +score2 ? 'success' : 'danger';
   }
 
-  goToDirections() {}
+  goToDirections(location: LocationData) {
+    window.location = `geo:${location.latitude},${location.longitude};u=35`;
+  }
 }
